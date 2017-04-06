d077afb84369cb6e68167ab3b8d9e0cdf97d8d25
Tomasz Smelcerz | hybris <tomasz.smelcerz@sap.com>
2017-04-06 22:12:24 +0200

    cleaning up

--- tts-service/web/create.go
+++ tts-service/web/create.go
@@ -2,54 +2,15 @@ package web
 
 import (
 	"net/http"
-
-	"github.com/SAPHybrisGliwice/golang-part-2/tts-service/service"
-	"errors"
-	"encoding/json"
 )
 
 func onCreateRequest(h createHandling, w http.ResponseWriter, r *http.Request) {
 
-	createDTO, inputErr := readCreateDTO(r)
-	if inputErr != nil {
-		handleError(inputErr, w, r)
-		return
-	}
+	//1 read create DTO
 
-	ttsCreate, validationErr := validateCreateDTO(createDTO)
-	if validationErr != nil {
-		handleError(validationErr, w, r)
-		return
-	}
+	//2 validate create DTO
 
-	//Invoke service
-	result, serviceErr := h.service.Create(*ttsCreate)
-	if serviceErr != nil {
-		message := ErrorDTO{500, serviceErr.Error(), nil}
-		handleError(message, w, r)
-		return
-	} else {
-		resultDTO := toResultDTO(result, h.mediaUrl)
-		json.NewEncoder(w).Encode(resultDTO)
-	}
-}
+	//3 Invoke service
 
-//Parses input data. Returns an error if unable to parse for any reason.
-func readCreateDTO(r *http.Request) (*CreateDTO, error) {
-	return nil, errors.New("Not implemented")
-}
-
-//Performs validation of the data.
-//If data is valid, returns service.TtsCreate object.
-//An error is returned otherwise.
-func validateCreateDTO(dto *CreateDTO) (*service.TtsCreate, error) {
-	var details []string
-
-	langEnum := service.EN
-
-	if len(details) == 0 {
-		return &service.TtsCreate{dto.Text, langEnum}, nil
-	} else {
-               return nil, ErrorDTO{400, "Invalid payload", details}
-	}
+	//4 send the result
 }
