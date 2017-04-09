29ca64a044eca0868ede16e08f66d3c5c0589a30
pssap <pawel.soltysek@sap.com>
2017-04-09 10:57:21 +0200

    Simplify code. Add more comments

--- tts-service/tts/converter.go
+++ tts-service/tts/converter.go
@@ -1,42 +1,50 @@
 package tts
 
 import (
-	"io"
+        "io"
+        "io/ioutil"
         "bytes"
 )
 
-type Converter interface {
+type converter interface {
 
-	//Convert converts the text to a media.
-	//It returns an io.Reader of an error, if any.
-	Convert(text string, metadata Metadata) (io.Reader, error)
+        // Convert converts the text to a media.
+        // It returns an io.ReadCloser of an error, if any.
+        Convert(text string, metadata Metadata) (io.ReadCloser, error)
 }
 
+// VoiceRss based implementation of the converter interface //
 type voiceRssConverter struct {
-	apiUrl string
+        apiUrl string
 }
 
-func (c voiceRssConverter) Convert(text string, meta Metadata) (io.Reader, error) {
+//  VoiceRSS http://www.voicerss.org/api/documentation.aspx
+//
+//  The API has the following URL: https://api.voicerss.org/
+//
+//  Parameters:
+//  key - The API key (mandatory)
+//  src - The textual content for converting to speech (length limited by 100KB) (mandatory)
+//  hl  - The textual content language. Allows values: see Languages (mandatory)
+//  f   - The speech audio formats. Allows values: see Audio Formats. Default value: 8khz_8bit_mono. (optional)
+//  r   - The speech rate (speed). Allows values: from -10 (slowest speed) up to 10 (fastest speed). Default value: 0 (normal speed). (optional)
+func (c voiceRssConverter) Convert(text string, meta Metadata) (io.ReadCloser, error) {
 
-        return new(bytes.Buffer), nil
+       return ioutil.NopCloser(new(bytes.Buffer)), nil
 }
 
 func newVoiceRssConverter() *voiceRssConverter {
 
-	return &voiceRssConverter{
-		apiUrl: "https://api.voicerss.org/",
-	}
+        return &voiceRssConverter{
+                apiUrl: "https://api.voicerss.org/",
+        }
 }
 
 func (c voiceRssConverter) resolveLang(lang string) string {
 
-	switch lang {
+        if lang == "PL" {
+                return "pl-pl"
+        }
 
-	case "PL":
-
-		return "pl-pl"
-	default:
-
-		return "en-us"
-	}
+        return "en-us"
 }
--- tts-service/tts/engine_test.go
+++ tts-service/tts/engine_test.go
@@ -1,115 +1,116 @@
 package tts
 
 import (
-	"errors"
-	. "github.com/smartystreets/goconvey/convey"
-	"io"
-	"strings"
-	"testing"
+        "errors"
+        . "github.com/smartystreets/goconvey/convey"
+        "io"
+        "strings"
+        "testing"
+        "io/ioutil"
 )
 
 func TestEngine(t *testing.T) {
 
-	Convey("TTS engine", t, func(c C) {
+        Convey("TTS engine", t, func(c C) {
 
-		Convey("Process method", func() {
+                Convey("Process method", func() {
 
-			Convey("should pass error from converter", func() {
+                        Convey("should pass error from converter", func() {
 
-				engine := newEngine(mockConverter{true}, mockStorage{false})
+                                engine := &Engine{mockConverter{true}, mockStorage{false}}
 
-				_, err := engine.Process("", Metadata{})
+                                _, err := engine.Process("", Metadata{})
 
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldEqual, converterErrorMessage)
-			})
+                                So(err, ShouldNotBeNil)
+                                So(err.Error(), ShouldEqual, converterErrorMessage)
+                        })
 
-			Convey("should pass error from storage", func() {
+                        Convey("should pass error from storage", func() {
 
-				engine := newEngine(mockConverter{false}, mockStorage{true})
+                                engine := &Engine{mockConverter{false}, mockStorage{true}}
 
-				_, err := engine.Process("", Metadata{})
+                                _, err := engine.Process("", Metadata{})
 
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldEqual, storageErrorMessage)
-			})
+                                So(err, ShouldNotBeNil)
+                                So(err.Error(), ShouldEqual, storageErrorMessage)
+                        })
 
-			Convey("should not blow if there are no errors", func() {
+                        Convey("should not blow if there are no errors", func() {
 
-				engine := newEngine(mockConverter{false}, mockStorage{false})
+                                engine := &Engine{mockConverter{false}, mockStorage{false}}
 
-				_, err := engine.Process("", Metadata{})
+                                _, err := engine.Process("", Metadata{})
 
-				So(err, ShouldBeNil)
-			})
-		})
+                                So(err, ShouldBeNil)
+                        })
+                })
 
-		Convey("GetResult method", func() {
+                Convey("GetResult method", func() {
 
-			Convey("should pass error from storage", func() {
+                        Convey("should pass error from storage", func() {
 
-				engine := newEngine(mockConverter{false}, mockStorage{true})
+                                engine := &Engine{mockConverter{false}, mockStorage{true}}
 
-				_, err := engine.Process("", Metadata{})
+                                _, err := engine.Process("", Metadata{})
 
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldEqual, storageErrorMessage)
-			})
+                                So(err, ShouldNotBeNil)
+                                So(err.Error(), ShouldEqual, storageErrorMessage)
+                        })
 
-			Convey("should not blow if there are no errors", func() {
+                        Convey("should not blow if there are no errors", func() {
 
-				engine := newEngine(mockConverter{false}, mockStorage{false})
+                                engine := &Engine{mockConverter{false}, mockStorage{false}}
 
-				_, err := engine.Process("", Metadata{})
+                                _, err := engine.Process("", Metadata{})
 
-				So(err, ShouldBeNil)
-			})
-		})
-	})
+                                So(err, ShouldBeNil)
+                        })
+                })
+        })
 }
 
 const converterErrorMessage = "Converter. Unexpected error."
 const storageErrorMessage = "Storage. Unexpected error."
 
 type mockConverter struct {
-	failing bool
+        failing bool
 }
 
-func (mc mockConverter) Convert(text string, metadata Metadata) (io.Reader, error) {
+func (mc mockConverter) Convert(text string, metadata Metadata) (io.ReadCloser, error) {
 
-	if mc.failing {
+        if mc.failing {
 
-		return nil, errors.New(converterErrorMessage)
-	}
+                return nil, errors.New(converterErrorMessage)
+        }
 
-	return strings.NewReader("test"), nil
+        return ioutil.NopCloser(strings.NewReader("test")), nil
 }
 
 type mockStorage struct {
-	failing bool
+        failing bool
 }
 
 func (ms mockStorage) Save(data io.Reader) (string, error) {
 
-	if ms.failing {
+        if ms.failing {
 
-		return "", errors.New(storageErrorMessage)
-	}
+                return "", errors.New(storageErrorMessage)
+        }
 
-	return "dummyID", nil
+        return "dummyID", nil
 }
 
-func (ms mockStorage) Get(id string) (io.Reader, error) {
+func (ms mockStorage) Get(id string) (io.ReadCloser, error) {
 
-	if ms.failing {
+        if ms.failing {
 
-		return nil, errors.New(storageErrorMessage)
-	}
+                return nil, errors.New(storageErrorMessage)
+        }
 
-	return strings.NewReader("test"), nil
+        return ioutil.NopCloser(strings.NewReader("test")), nil
 }
 
 func (ms mockStorage) Delete(id string) error {
 
-	return nil
+        return nil
 }
